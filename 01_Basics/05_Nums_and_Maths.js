74b49c7be83a5c8aed6eab16be02aaf878fe2051
Your NameHasanKabir77 <you@example.comhasankabir0720@gmail.com>
2024-04-16 14:55:27 +0600

    Math and Number in JS

---
+++ 01_Basics/05_Nums_and_Maths.js
@@ -0,0 +1,25 @@
+// const score = 150
+// console.log(score);
+// const balance = new Number(150)
+// console.log(balance);
+
+// const hundrads = 1000000
+
+// console.log(hundrads.toLocaleString('en-BN'));
+
+// ------------------- Maths -------------------
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.62));
+// console.log(Math.ceil(4.652));
+// console.log(Math.floor(4.652));
+
+console.log(Math.min(4,5,9,7,6,2));
+
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+
+const min = 5
+const max = 20
+console.log(Math.random() * (max - min + 1) + min)
