3d4dd7f58dcbadef6aa6161575b59803eadc61d0
Your NameHasanKabir77 <you@example.comhasankabir0720@gmail.com>
2024-04-17 17:50:07 +0600

    Complete Objects 2nd Part

---
+++ 02_Basics/04_ObjectsPart_2.js
@@ -0,0 +1,37 @@
+const SocialMediaUser = {}
+
+SocialMediaUser.id = "1326A"
+SocialMediaUser.Name = "Mashrafee"
+SocialMediaUser.Email = "mash213@gmail.com"
+SocialMediaUser.isLoggedIn = true
+
+// console.log(SocialMediaUser);
+
+ const regularUser = {
+    userEmail: "user213@gmail.com",
+
+        fullName: {
+            userFullName: {
+                firstname: "Hasan",
+                lastName: "Kabir"
+            }   
+        }
+ }
+//  console.log(regularUser.fullName?.userFullName);
+
+const obj1 = {1: "Hasan", 2: "Sharif"}
+const obj2 = {3: "Monir", 4: "Shuvo"}
+
+// let obj3 = {obj1, obj2}
+// const obj3 = Object.assign({}, obj1, obj2)
+
+const obj3 = {...obj1, ...obj2}
+
+// console.log(obj3); 
+
+// console.log(Object.keys(SocialMediaUser));
+// console.log(Object.keys(regularUser));
+
+
+console.log(Object.values(SocialMediaUser));
+console.log(Object.values(regularUser));
