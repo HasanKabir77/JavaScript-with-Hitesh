6e74e1e99be5ccbd6dd1d8d1573016fc34639c4e
Your NameHasanKabir77 <you@example.comhasankabir0720@gmail.com>
2024-04-21 16:49:10 +0600

    Learn Scope Level and mini Hosting in JS

--- 03_Basics/02_Scope.js
+++ 03_Basics/02_Scope.js
@@ -7,9 +7,55 @@ if(true)
 {
     let a = 10
     const b = 20
-    console.log("Inner : ", a);
+    // console.log("Inner : ", a);
     // var c = 30
 }
 // console.log(a);
 // console.log(b);
-console.log(a);
+// console.log(a);
+
+//  ---------------------- Video 22 ----------------------
+
+//  ---------------------- Scope (Details) ----------------------
+
+function one() {
+    const username = "Hasan"
+
+    function two() {
+        const website = "Facebook"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+// one()
+
+
+if(true)
+{
+    const username = "Kabir"
+    if(username === "Kabir")
+    {
+        const website = " Youtube"
+        // console.log(username + website);
+    }
+    // console.log(website); // Error
+}
+
+// console.log(username); // Error
+
+//  console.log(addOne(5)); // it can be use before initialize
+
+function addOne(num) 
+{
+    return num + 1    
+}
+console.log(addOne(5));
+
+// console.log(addTwo(5)); // can't use before intialize
+
+const addTwo = function(num)
+{
+    return num + 2
+}
+console.log(addTwo(5));
