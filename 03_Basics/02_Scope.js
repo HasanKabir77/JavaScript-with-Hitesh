7689b5cac972285af2cd38c45f8c9a5ba98e70ce
Your NameHasanKabir77 <you@example.comhasankabir0720@gmail.com>
2024-04-21 16:26:40 +0600

    Learn Scope in JS Video No. 20

---
+++ 03_Basics/02_Scope.js
@@ -0,0 +1,15 @@
+//  ---------------------- Video 21 ----------------------
+
+//  ---------------------- Scope ----------------------
+
+let a = 100
+if(true)
+{
+    let a = 10
+    const b = 20
+    console.log("Inner : ", a);
+    // var c = 30
+}
+// console.log(a);
+// console.log(b);
+console.log(a);
